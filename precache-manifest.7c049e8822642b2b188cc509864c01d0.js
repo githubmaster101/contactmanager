7233940db1c7278a2b82d033a0a4c0574467f136
Kevin <yardman10550@gmail.com>
2019-01-20 22:05:26 -0500

    Updates

---
+++ precache-manifest.7c049e8822642b2b188cc509864c01d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "85584d56b18ce0c86760",
+    "url": "/contactmanager/static/js/main.85584d56.chunk.js"
+  },
+  {
+    "revision": "052b43a5c9afed71c19c",
+    "url": "/contactmanager/static/js/1.052b43a5.chunk.js"
+  },
+  {
+    "revision": "85584d56b18ce0c86760",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "052b43a5c9afed71c19c",
+    "url": "/contactmanager/static/css/1.4427480f.chunk.css"
+  },
+  {
+    "revision": "f28596edd22f89f7de370bca2688a02c",
+    "url": "/contactmanager/index.html"
+  }
+];
